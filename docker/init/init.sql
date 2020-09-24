d57c77a1ea166e13be9e469a9130c070576d2468
narigasawa_shu <narigasawa_shu@interprism.co.jp>
2020-09-25 00:51:08 +0900

    postgreのdocker環境の追加

---
+++ docker/init/init.sql
@@ -0,0 +1,16 @@
+create table messages (
+  id serial primary key,
+  user_id varchar not null,
+  title varchar not null,
+  url varchar not null
+);
+
+create table messageUsers (
+  message_id int not null,
+  user_id varchar not null,
+  status int not null,
+  primary key(message_id, user_id)
+);
+
+GRANT ALL PRIVILEGES ON messages To admin;
+GRANT ALL PRIVILEGES ON messageUsers To admin;
